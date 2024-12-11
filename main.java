d1e3daaf5dd9c4c5ed1e3f14099d6be4baf81e78
Pete <pflem121@gmail.com>
2024-12-10 18:16:59 -0600

    Initialize and comment

---
+++ main.java
@@ -0,0 +1,15 @@
+Class Main{
+
+    public static void main(String[] args){
+        //Generate 2 Players, Deck of cards and deal their hands
+
+        //Core game Logic
+        /// Players play cards
+        /// Compare cards
+        /// Winner adds cards to their discardDeck
+        /// If mainDeck is empty, shuffle discardDeck and add cards to mainDeck
+
+
+        //Implement "Wars" on ties
+    }
+}
