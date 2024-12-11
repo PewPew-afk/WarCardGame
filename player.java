39a4fc8fd2acc36bda27a6a46b241d9ab77a2f3c
PewPew-afk <pflem121@gmail.com>
2024-12-11 12:37:12 -0600

    Add getter/setter for name.java

--- player.java
+++ player.java
@@ -1,14 +1,13 @@
 Class Player{
   public class Player(){
-    public String name;
+    private String name;
     private Hand hand;
     private Hand discardHand;
+
     
     public Card playCard(Hand hand){
       return hand.topCard()
     }
-
-      
     public Hand getHand(){
       return hand;
     }
@@ -20,6 +19,12 @@ Class Player{
     }
     public void setDiscardHand(Hand discardHand){
       this.discardHand = discardHand;
+    }
+    public String getName(){
+      return name;
+    }
+    public void setName(String name){
+      this.name = name;
     }
     
   }
